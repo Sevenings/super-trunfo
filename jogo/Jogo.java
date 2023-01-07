29691c614f4dc9830cea4d91128fc4bcd7faf6a8
Sevenings <lvlassis.2@gmail.com>
2023-01-07 18:13:17 -0300

    jogo play begin

--- jogo/Jogo.java
+++ jogo/Jogo.java
@@ -5,12 +5,17 @@ import jogador.*;
 
 public class Jogo {
     private List<Jogador> jogadores;
+    private Jogador vez;
+    private Jogador vencedor;
 
     public Jogo(List<Jogador> jogadores) {
         this.jogadores = jogadores;
     }
 
     public Jogador play() {
+        Random random = new Random();
+        vez = jogadores.get(random.nextInt(jogadores.size())); 
+
         return vencedor;
     }
 }
